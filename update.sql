537362eab9044fa17fa54ba6ffbb5a01687e484d
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2018-12-03 15:01:04 +0100

    Update update.sql

--- update.sql
+++ update.sql
@@ -1,1 +1,18 @@
+UPDATE student
+SET major = 'Undecided';
 
+UPDATE student
+SET name = 'Johnny'
+WHERE student_id = 4;
+
+UPDATE student
+SET major = 'Biological Sciences'
+WHERE major = 'Biology';
+
+UPDATE student
+SET major = 'Biosociology'
+WHERE major = 'Biology' OR major = 'sociology'
+
+UPDATE student
+SET major = 'Undecided', name = 'Tom'
+WHERE student_id = 4;
