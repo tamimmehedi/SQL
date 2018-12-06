aff5551a795a285342da74b45a5e2109bb2c8bde
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2018-12-06 15:30:56 +0100

    Update BasicQueries2.sql

--- BasicQueries2.sql
+++ BasicQueries2.sql
@@ -1,1 +1,40 @@
 
+-- Find all employees
+SELECT *
+FROM employee;
+
+-- Find all clients
+SELECT *
+FROM clients;
+
+-- Find all employees ordered by salary
+SELECT *
+from employee
+ORDER BY salary ASC/DESC;
+
+-- Find all employees ordered by sex then name
+SELECT *
+from employee
+ORDER BY sex, name;
+
+-- Find the first 5 employees in the table
+SELECT *
+from employee
+LIMIT 5;
+
+-- Find the first and last names of all employees
+SELECT first_name, employee.last_name
+FROM employee;
+
+-- Find the forename and surnames names of all employees
+SELECT first_name AS forename, employee.last_name AS surname
+FROM employee;
+
+-- Find out all the different genders
+SELECT DISCINCT sex
+FROM employee;
+
+-- Find all male employees
+SELECT *
+FROM employee
+WHERE sex = 'M';
