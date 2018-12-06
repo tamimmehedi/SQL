4fe66a50ad7c738ef1d60eac2b2fb6bd6a27124f
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2018-12-06 15:31:40 +0100

    Update BasicQueries3.sql

--- BasicQueries3.sql
+++ BasicQueries3.sql
@@ -1,1 +1,29 @@
+-- Find all employees at branch 2
+SELECT *
+FROM employee
+WHERE branch_id = 2;
 
+-- Find all employee's id's and names who were born after 1969
+SELECT emp_id, first_name, last_name
+FROM employee
+WHERE birth_day >= 1970-01-01;
+
+-- Find all female employees at branch 2
+SELECT *
+FROM employee
+WHERE branch_id = 2 AND sex = 'F';
+
+-- Find all employees who are female & born after 1969 or who make over 80000
+SELECT *
+FROM employee
+WHERE (birth_day >= '1970-01-01' AND sex = 'F') OR salary > 80000;
+
+-- Find all employees born between 1970 and 1975
+SELECT *
+FROM employee
+WHERE birth_day BETWEEN '1970-01-01' AND '1975-01-01';
+
+-- Find all employees named Jim, Michael, Johnny or David
+SELECT *
+FROM employee
+WHERE first_name IN ('Jim', 'Michael', 'Johnny', 'David');
