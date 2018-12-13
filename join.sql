05906979fcadc21d16f34389609cf2e57373eaa6
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2018-12-13 20:56:06 +0100

    Update join.sql

--- join.sql
+++ join.sql
@@ -3,5 +3,5 @@ INSERT INTO branch VALUES(4, "Buffalo", NULL, NULL);
 
 SELECT employee.emp_id, employee.first_name, branch.branch_name
 FROM employee
-JOIN branch    -- LEFT JOIN, RIGHT JOIN
+JOIN branch -- LEFT JOIN, RIGHT JOIN
 ON employee.emp_id = branch.mgr_id;
