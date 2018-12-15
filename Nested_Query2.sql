0bfbdd96ffe7a60e3555a560ff5a8d58b5109c88
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2018-12-15 21:11:33 +0100

    Update and rename NestedQuery2.sql to Nested_Query2.sql

---
+++ Nested_Query2.sql
@@ -0,0 +1,20 @@
+-- Find the names of employees who work with clients handled by the scranton branch
+SELECT employee.first_name, employee.last_name
+FROM employee
+WHERE employee.emp_id IN (
+                         SELECT works_with.emp_id
+                         FROM works_with
+                         )
+AND employee.branch_id = 2;
+
+-- Find the names of all clients who have spent more than 100,000 dollars
+SELECT client.client_name
+FROM client
+WHERE client.client_id IN (
+                          SELECT client_id
+                          FROM (
+                                SELECT SUM(works_with.total_sales) AS totals, client_id
+                                FROM works_with
+                                GROUP BY client_id) AS total_client_sales
+                          WHERE totals > 100000
+);
